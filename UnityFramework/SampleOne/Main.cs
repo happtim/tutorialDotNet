54fcfabe99d8bae47c69ce8ec749fd2f05585961
timge <gexiang@gmail.com>
2017-05-19 13:34:04 +0800

    生命周期管理

--- UnityFramework/SampleOne/Main.cs
+++ UnityFramework/SampleOne/Main.cs
@@ -12,6 +12,7 @@ namespace UnityFramework.SampleOne
     [TestClass]
     public class Main
     {
+        //构造器注入
         [TestMethod()]
         public void ConstructInject()
         {
@@ -22,6 +23,7 @@ namespace UnityFramework.SampleOne
 
         }
 
+        //属性注入
         [TestMethod()]
         public void PropertyInject() {
             UnityContainer container = new UnityContainer();
@@ -30,11 +32,48 @@ namespace UnityFramework.SampleOne
             people.DrinkWater();
         }
 
+        //方法注入
         [TestMethod()]
         public void methodInject() {
             UnityContainer container = new UnityContainer();
             container.RegisterType<IWaterTool, SeaWater>();
             IPeople people = container.Resolve<ShipPeople>();
         }
+
+        //上面方法都使用接口作为对象标识符
+        //当有多个接口注册时就是不知道用哪个了，所以使用标志符标识
+        [TestMethod()]
+        public void Identify() {
+            UnityContainer container = new UnityContainer();
+            container.RegisterType<IWaterTool, CityWater>("CityWatter");
+            container.RegisterType<IWaterTool, PressWater>("PressWater");
+
+            IWaterTool cityWater = container.Resolve<IWaterTool>("CityWatter");
+            Console.WriteLine(cityWater.GetWater());
+
+            var list = container.ResolveAll<IWaterTool>();
+        }
+
+        //Resolve使用暂时的生命周期，如果想生成持久的使用ContainerControlledLifetimeManager
+        [TestMethod()]
+        public void lifetime() {
+
+            UnityContainer container = new UnityContainer();
+            container.RegisterType<IWaterTool, SeaWater>("SeaWater",new TransientLifetimeManager());
+
+            IWaterTool left  = container.Resolve<IWaterTool>("SeaWater");
+            IWaterTool right = container.Resolve<IWaterTool>("SeaWater");
+
+            Assert.AreNotEqual(left, right);
+
+            container.RegisterType<IWaterTool, PressWater>("PressWater", new ContainerControlledLifetimeManager());
+            left = container.Resolve<IWaterTool>("PressWater");
+            right = container.Resolve<IWaterTool>("PressWater");
+
+            Assert.AreEqual(left, right);
+
+        }
+
+
     }
 }
