27b17e6a04d1e2a7b9937404989d5fbb80c33624
timge <gexiang@gmail.com>
2017-07-01 10:07:40 +0800

    配置log4net

---
+++ Log4Net/AppConfig/AppConfigTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Log4Net.Com;
+
+//log4net 配置文件 默认使用Log4net.exe.config 配置文件
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace Log4Net.AppConfig
+{
+    [TestClass]
+    public class AppConfigTest
+    {
+
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        [TestMethod]
+        public void Test() {
+            log.Info("Entering application.");
+            Bar bar = new Bar();
+            bar.DoIt();
+            log.Info("Exiting application.");
+        }
+    }
+}
---
+++ Log4Net/AttributeConfig/AttributeConfigTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Log4Net.Com;
+
+//log4net 配置文件 ConfigFile找相对路径于开始程序的地方
+//[assembly:log4net.Config.XmlConfigurator(Watch =true,ConfigFile ="AttributeConfig/App1.config")]
+
+
+namespace Log4Net.AttributeConfig
+{
+    [TestClass]
+    public class AttributeConfigTest
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        [TestMethod]
+        public void Test() {
+
+            log.Info("Entering application.");
+            Bar bar = new Bar();
+            bar.DoIt();
+            log.Info("Exiting application.");
+
+        }
+    }
+}
