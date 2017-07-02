11c8ea4b31a3cc4cebda89f8e1d0ff0da8a678a9
timge <gexiang@gmail.com>
2017-07-02 16:32:25 +0800

    context

---
+++ Log4Net/Contexts/ContextTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+using log4net.Config;
+using log4net;
+
+using Log4Net.Com;
+
+namespace Log4Net.Contexts
+{
+    [TestClass]
+    public class ContextTest
+    {
+        //使用appConfig setting
+        private static readonly ILog log = LogManager.GetLogger(typeof(ContextTest));
+
+        [TestMethod]
+        public void Test() {
+
+            //输出 layout  %property{something}
+            GlobalContext.Properties["something"] = "agv";
+
+            log.Info("Entering application.");
+            Bar bar = new Bar();
+            bar.DoIt();
+            log.Info("Exiting application.");
+
+            new Thread(new ThreadStart(delegate()
+            {
+                ThreadContext.Properties["something"] = "thread_agv";
+                log.Info("Entering application.");
+                Bar bar2 = new Bar();
+                bar2.DoIt();
+                log.Info("Exiting application.");
+
+            })).Start();
+        }
+
+    }
+}
