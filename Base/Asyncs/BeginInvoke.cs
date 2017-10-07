14bd2f46b64689dc41f05cd9b2441954aa697149
timge <gexiang@agvsz.com>
2017-10-07 19:07:03 +0800

    异步调用

---
+++ Base/Asyncs/BeginInvoke.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Base.Asyncs
+{
+
+    public class AsyncDemo {
+
+        // The method to be executed asynchronously.
+        public string TestMethod(int callDuration, out int threadId)
+        {
+            Console.WriteLine("Test method begins.");
+            Thread.Sleep(callDuration);
+            threadId = Thread.CurrentThread.ManagedThreadId;
+            return String.Format("My call time was {0}.", callDuration.ToString());
+        }
+    }
+    // The delegate must have the same signature as the method
+    // it will call asynchronously.
+    public delegate string AsyncMethodCaller(int callDuration, out int threadId);
+
+    [TestClass]
+    public class BeginInvoke {
+
+        [TestMethod]
+        public void Test() {
+
+            // The asynchronous method puts the thread id here.
+            int threadId;
+
+            // Create an instance of the test class.
+            AsyncDemo ad = new AsyncDemo();
+
+            // Create the delegate.
+            AsyncMethodCaller caller = new AsyncMethodCaller(ad.TestMethod);
+
+            // Initiate the asychronous call.
+            IAsyncResult result = caller.BeginInvoke(3000,
+                out threadId, null, null);
+
+            Thread.Sleep(0);
+            Console.WriteLine("Main thread {0} does some work.",
+                Thread.CurrentThread.ManagedThreadId);
+
+            // Call EndInvoke to wait for the asynchronous call to complete,
+            // and to retrieve the results.
+            //两种获取值得方式,一个使用out一个使用返回值
+            string returnValue = caller.EndInvoke(out threadId, result);
+
+            Console.WriteLine("The call executed on thread {0}, with return value \"{1}\".",
+                    threadId, returnValue);
+        }
+      
+    }
+
+}
---
+++ Base/Asyncs/BlockUntilOperationCompletes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Base.Asyncs
+{
+    [TestClass]
+    public class BlockUntilOperationCompletes {
+
+        [TestMethod]
+        public void Test() {
+
+            string args = "127.0.0.1";
+            // Start the asynchronous request for DNS information.
+            // This example does not use a delegate or user-supplied object
+            // so the last two arguments are null.
+            IAsyncResult result = Dns.BeginGetHostEntry(args, null, null);
+            Console.WriteLine("Processing your request for information...");
+            // Do any additional work that can be done here.
+            try
+            {
+                // EndGetHostByName blocks until the process completes.
+                IPHostEntry host = Dns.EndGetHostEntry(result);
+                string[] aliases = host.Aliases;
+                IPAddress[] addresses = host.AddressList;
+                if (aliases.Length > 0)
+                {
+                    Console.WriteLine("Aliases");
+                    for (int i = 0; i < aliases.Length; i++)
+                    {
+                        Console.WriteLine("{0}", aliases[i]);
+                    }
+                }
+                if (addresses.Length > 0)
+                {
+                    Console.WriteLine("Addresses");
+                    for (int i = 0; i < addresses.Length; i++)
+                    {
+                        Console.WriteLine("{0}", addresses[i].ToString());
+                    }
+                }
+            } catch (SocketException e)
+            {
+                Console.WriteLine("An exception occurred while processing the request: {0}", e.Message);
+            }
+        }
+
+    }
+}
