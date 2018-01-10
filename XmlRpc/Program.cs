0bda96a99f4ab93debd0da2c5f7b6fbe49f8e2bd
Timge <xiang.ge@agvsz.com>
2018-01-10 11:19:40 +0800

    xmlrpc

---
+++ XmlRpc/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CookComputing.XmlRpc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XmlRpc
+{
+    [TestClass]
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        [TestMethod]
+        public void TestClient() {
+            fandai fd = XmlRpcProxyGen.Create<fandai>();
+            int a = fd.findai(1);
+            Console.WriteLine(a);
+        }
+
+    }
+}
---
+++ XmlRpc/fandai.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CookComputing.XmlRpc;
+/*
+//python xmlrpc server
+
+from SimpleXMLRPCServer import SimpleXMLRPCServer  
+global a  
+  
+def load():  
+    global a  
+    a = [1 ,2, 24]  
+    return a  
+  
+def findai(i):  
+    global a  
+    print a[i]  
+    return a[i]  
+  
+server = SimpleXMLRPCServer(("localhost", 8000))  
+server.register_function(findai,"findai")  
+load()  
+server.serve_forever()   
+*/
+
+namespace XmlRpc
+{
+    [XmlRpcUrl("http://localhost:8000/")]
+    public interface fandai : IXmlRpcProxy {
+
+        [XmlRpcMethod]
+        int findai(int i);
+    }
+}
