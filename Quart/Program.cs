b91553871f5ddae1b43091bfcb52eedaaea9f2c0
timge <xiang.ge@agvsz.com>
2018-11-06 13:35:31 +0800

    Quartz Framewrok

---
+++ Quart/Program.cs
@@ -0,0 +1,81 @@
+﻿using Quartz;
+using Quartz.Impl;
+using Quartz.Impl.Calendar;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Quart
+{
+    class Program
+    {
+        static  void  Main(string[] args)
+        {
+            try {
+                Run();
+            } catch (Exception ex) {
+                Console.WriteLine(ex);
+            }
+
+
+            AutoResetEvent ev = new AutoResetEvent(false);
+            ev.WaitOne();
+        }
+
+        public static async void Run()
+        {
+            Console.WriteLine("------- Initializing ----------------------");
+
+            // First we must get a reference to a scheduler
+            var properties = new NameValueCollection
+            {
+                ["quartz.scheduler.instanceName"] = "XmlConfiguredInstance",
+                ["quartz.threadPool.type"] = "Quartz.Simpl.SimpleThreadPool, Quartz",
+                ["quartz.threadPool.threadCount"] = "10",
+                ["quartz.plugin.xml.type"] = "Quartz.Plugin.Xml.XMLSchedulingDataProcessorPlugin, Quartz.Plugins",
+                ["quartz.plugin.xml.fileNames"] = "~/quartz_jobs.xml",
+                // this is the default
+                ["quartz.plugin.xml.FailOnFileNotFound"] = "true",
+                // this is not the default
+                ["quartz.plugin.xml.failOnSchedulingError"] = "true"
+            };
+
+            // set thread pool info
+
+            // job initialization plugin handles our xml reading, without it defaults are used
+
+            ISchedulerFactory sf = new StdSchedulerFactory(properties);
+            IScheduler sched = await sf.GetScheduler();
+
+            // we need to add calendars manually, lets create a silly sample calendar
+            //var dailyCalendar = new DailyCalendar("00:01", "23:59");
+            //dailyCalendar.InvertTimeRange = true;
+            //await sched.AddCalendar("cal1", dailyCalendar, false, false);
+
+            Console.WriteLine("------- Initialization Complete -----------");
+
+            // all jobs and triggers are now in scheduler
+
+            // Start up the scheduler (nothing can actually run until the
+            // scheduler has been started)
+            await sched.Start();
+            Console.WriteLine("------- Started Scheduler -----------------");
+
+            // wait long enough so that the scheduler as an opportunity to
+            // fire the triggers
+            Console.WriteLine("------- Waiting 30 seconds... -------------");
+
+            await Task.Delay(30 * 1000);
+
+            // shut down the scheduler
+            Console.WriteLine("------- Shutting Down ---------------------");
+            await sched.Shutdown(true);
+
+            Console.WriteLine("------- Shutdown Complete -----------------");
+        }
+    }
+}
---
+++ Quart/SimpleJob.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using log4net;
+using Quartz;
+using Quartz.Logging;
+
+namespace Quart
+{
+    /// <summary>
+    /// This is just a simple job that gets fired off many times by example 15.
+    /// </summary>
+    /// <author>Bill Kratzer</author>
+    /// <author>Marko Lahma (.NET)</author>
+    public class SimpleJob : IJob
+    {
+        //private static readonly ILog log = LogProvider.GetLogger(typeof(SimpleJob));
+
+        /// <summary>
+        /// Called by the <see cref="IScheduler" /> when a
+        /// <see cref="ITrigger" /> fires that is associated with the <see cref="IJob" />.
+        /// </summary>
+        public virtual Task Execute(IJobExecutionContext context)
+        {
+            // This job simply prints out its job name and the
+            // date and time that it is running
+            JobKey jobKey = context.JobDetail.Key;
+            Console.WriteLine("SimpleJob says: {0} executing at {1}", jobKey, DateTime.Now.ToString("r"));
+            //return TaskUtil.CompletedTask;
+            return Task.FromResult(result: true);
+        }
+    }
+}
