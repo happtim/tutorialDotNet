c01df80c60ee339d7af6f269df199a22858756c0
TimGe <gexiang@agvsz.com>
2020-03-24 14:33:51 +0800

    Dotfuscated

---
+++ SampleDotfuscator/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDotfuscator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            int a = 1;
+            int b = a + 100;
+            Console.WriteLine( string.Format("Hello World! b = {0}",b));
+            Console.ReadKey();
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+    }
+}
